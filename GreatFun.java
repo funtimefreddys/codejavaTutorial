b2f806d6238c557c1500a483ee288047c1e24752
GuskungTH <82573786+funtimefreddys@users.noreply.github.com>
2024-07-25 14:35:53 +0700

    Create GreatFun.java

---
+++ GreatFun.java
@@ -0,0 +1,8 @@
+public class GreatFun {
+    public static void main(String args[]){
+        int x,y;
+        x = 10;
+        y = x+1;
+        System.out.println("x = " +x+ "y = " +y);
+    }
+}
