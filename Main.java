7a14bdd47e6a0d0e6d76f04bde0544e5c872c14b
GuskungTH <82573786+funtimefreddys@users.noreply.github.com>
2024-07-25 14:36:00 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a,b,total;
+        a = 62;
+        b = 99;
+        total = a+b;
+
+       
+        System.out.println("a + b = "+total);
+    }
+}
