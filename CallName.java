2a88a2ff71e6c5a1cb5b2e8a2a394ef1f1b4766b
GuskungTH <82573786+funtimefreddys@users.noreply.github.com>
2024-08-02 10:16:39 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,10 @@
+public class CallName {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            displayMessage();
+        }
+    }
+    public static void displayMessage() {
+        System.out.println("Nipitpon Mangsung");
+    }
+}
